886716f9789d39ddeb9d039abed20630250ab618
remy <remypoc@free.fr>
2017-12-05 17:43:01 +0100

    Add page to manage all contacts (crud)

--- src/mvc/beans/contact/ContactManagerBean.java
+++ src/mvc/beans/contact/ContactManagerBean.java
@@ -1,6 +1,8 @@
 package mvc.beans.contact;
 
+import domain.Address;
 import domain.Contact;
+import domain.PhoneNumber;
 import service.ContactService;
 
 import javax.annotation.PostConstruct;
@@ -9,20 +11,35 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.model.DataModel;
 import javax.faces.model.ListDataModel;
 import java.io.Serializable;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 @ManagedBean(name = "contactManager")
 @ViewScoped
-public class ContactManagerBean implements Serializable{
+public class ContactManagerBean implements Serializable {
 
     private List<Contact> contacts;
     private transient DataModel<Contact> model;
+    private Contact contact = new Contact();
+    private Address address = new Address();
+    private LinkedHashSet<PhoneNumber> phones = new LinkedHashSet<>();
+    private boolean edit;
 
     @PostConstruct
     public void init() {
+        contact.setAddress(address);
+        contact.setPhones(phones);
         final ContactService cs = new ContactService();
         contacts = cs.loadContacts();
-        System.out.println("Display contacts : " + contacts);
+    }
+
+    // Reset placeholder.
+    public void reset() {
+        contact = new Contact();
+        address = new Address();
+        phones = new LinkedHashSet<>();
+        contact.setAddress(address);
+        contact.setPhones(phones);
     }
 
     public List<Contact> getContacts() {
@@ -33,10 +50,82 @@ public class ContactManagerBean implements Serializable{
         this.contacts = contacts;
     }
 
+    public void add() {
+        if (!contact.getAddress().isValid()) {
+            contact.setAddress(null);
+        }
+        final ContactService cs = new ContactService();
+        final Object lError = cs.addContact(contact);
+        contacts.add(contact);
+        reset();
+    }
+
+    public void setModel(DataModel<Contact> model) {
+        this.model = model;
+    }
+
     public DataModel<Contact> getModel() {
         if (model == null) {
             model = new ListDataModel<>(contacts);
         }
         return model;
+    }
+
+    public void edit() {
+        final ContactService cs = new ContactService();
+        final Object lError = cs.loadContact(model.getRowData().getId());
+        if (lError instanceof Contact) {
+            contact = (Contact) lError;
+        }
+        if (contact.getAddress() == null) {
+            contact.setAddress(address);
+        }
+        if (contact.getPhones() == null) {
+            contact.setPhones(phones);
+        }
+
+        edit = true;
+    }
+
+    public String save() {
+        if (contact.getAddress() != null && !contact.getAddress().isValid()) {
+            contact.setAddress(null);
+        }
+        final ContactService cs = new ContactService();
+        final Object lError = cs.updateContact(contact);
+        reset();
+        edit = false;
+        return "home";
+    }
+
+    public void delete() {
+        final ContactService cs = new ContactService();
+        final Object lError = cs.deleteContact(model.getRowData().getId());
+        contacts.remove(model.getRowData());
+        edit = false;
+        reset();
+    }
+
+    public Contact getItem() {
+        return contact;
+    }
+
+    public boolean isEdit() {
+        return edit;
+    }
+
+    public void cancel() {
+        reset();
+        edit = false;
+    }
+
+    public void addPhone() {
+        if (contact.getPhones().size() < 100) {
+            contact.getPhones().add(new PhoneNumber());
+        }
+    }
+
+    public void remove(PhoneNumber phoneNumber) {
+        contact.getPhones().remove(phoneNumber);
     }
 }
